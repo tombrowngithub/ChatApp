c89ad225e49655b358b46e7de15c963f57235cfa
Anuma Thomas <tombrownanuma@gmail.com>
2023-01-17 21:45:26 +0100

    Amended

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,28 +1,32 @@
 import React from "react";
 import GoogleSignIn from "../img/btn_google_signin_dark_pressed_web.png";
-import { auth } from "../firebase";
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import {auth, provider} from "../firebase";
+import {signInWithPopup} from "firebase/auth"
 
 const Welcome = () => {
-  const googleSignIn = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithRedirect(auth, provider);
-  };
 
-  return (
-    <main className="welcome">
-      <h2>Welcome to T-Chat.</h2>
-      <p>Sign in with Google to start chat.</p>
-      <button className="sign-in">
-        <img
-          onClick={googleSignIn}
-          src={GoogleSignIn}
-          alt="sign in with google"
-          type="button"
-        />
-      </button>
-    </main>
-  );
+    const googleSignIn = () => {
+        signInWithPopup(auth, provider)
+        // setLoading(true)
+        // const provider = new GoogleAuthProvider();
+        // signInWithRedirect(auth, provider).then(r => setLoading(false));
+    };
+
+    return (
+        <main className="welcome">
+            <h2>Welcome to T-Chat.</h2>
+            <p>Sign in with Google to start chat.</p>
+
+            <button className="sign-in">
+                <img
+                    onClick={googleSignIn}
+                    src={GoogleSignIn}
+                    alt="sign in with google"
+                    type="button"
+                />
+            </button>
+        </main>
+    );
 };
 
 export default Welcome;
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -15,4 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider()
 export const db = getFirestore(app);
